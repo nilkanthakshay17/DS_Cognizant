6872939a4882bb8353ac589cc97e37e85ca4ee90
Akshay Nilkanth <nilkanthakshay17@gmail.com>
2022-08-25 22:33:43 +0530

    Added test class

---
+++ Arrays_DS_Cognizant/src/TestBasicArray.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class TestBasicArray {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5,6};
+
+        for(int i: arr)
+            System.out.print(i+" ");
+    }
+
+}
